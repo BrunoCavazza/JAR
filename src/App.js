067b4b7e5b7832225158887a4eb24f6ddbcf3221
BrunoCavazza <anima@DESKTOP-EJTMQG1.localdomain>
2021-09-02 20:50:19 -0300

    Responsive Mobile & Desktop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import './App.css';
+import Resposive from './components/Responsive';
 
 function App() {
+  
   return (
+    
     <div className="App">
+       <Resposive />
       <header className="App-header">
-        
+     
       </header>
     </div>
   );
 }
 
 export default App;
+
+
